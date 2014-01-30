11ba6e428ac9a7d386e21cd980d6c95dd2b38ebd
Christopher Eyre <chriseyre2000@gmail.com>
2014-01-30 21:19:10 +0000

    Implemented Simple WebAPI

---
+++ SimpleSurveyAPI/SimpleSurveyAPI/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSurveyAPI.Controllers
+{
+    public class SurveyController : ApiController
+    {
+        public string Get(int id)
+        {
+            if (id == 1)
+            {
+                return "<span>First</span>";
+            }
+
+            if (id == 42)
+            {
+                return "<span>Hitchickers</span>";
+            }
+
+            return "<span>Unknown</span>";
+        }
+    }
+}
